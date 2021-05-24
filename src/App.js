abd2821ebd3bf8e26f5aab176c32b21094ec9822
Love to code my life <47113916+vaishnavan@users.noreply.github.com>
2021-05-24 20:08:12 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const mybutton = {
+    color:"blue",
+    backgroundColor:"black"
 }
 
-export default App;
+class App extends Component {
+    constructor(){
+      super();
+      var testObject = [
+        {title: "Pumpkin Pie", ingredients: ["Pumpkin Puree", "Sweetened Condensed Milk", "Eggs", "Pumpkin Pie Spice", "Pie Crust"]},
+        {title: "Spaghetti", ingredients: ["Noodles", "Tomato Sauce", "(Optional) Meatballs"]},
+        {title: "Onion Pie", ingredients: ["Onion", "Pie Crust", "Sounds Yummy right?"]}
+      ];
+  
+    localStorage.setItem('testObject', JSON.stringify(testObject));
+  
+      this.state = {
+        apple:localStorage.getItem('testObject')
+      }
+      alert(JSON.parse(localStorage.getItem('testObject')));
+  }
+  
+    render() {
+      return (
+        <div className="App">
+        </div>
+      );
+    }
+  }
+  
+  export default App;
